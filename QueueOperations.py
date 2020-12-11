93ed98d1f1f32432b10722b8c499dd26ac583e18
Mekkawy <Mekkawy@gmail.com>
2020-12-11 11:05:32 +0200

    final

--- QueueOperations.py
+++ QueueOperations.py
@@ -1,5 +1,5 @@
 
-from sympy import Eq,solve,symbols
+from sympy import *
 import math
 def modeChecker(pattern):
     pat = pattern.split('/')
@@ -39,6 +39,7 @@ class Deter:
 
         self.ti += (1/self.lambdda)
         self.ti=int(self.ti)
+        
 
     # ti case2 self.lambdda <= self.meu
 
@@ -57,35 +58,52 @@ class Deter:
     
 
     # find nt
-    def nT(self):
-        if(self.lambdda > self.meu):
-            return self.nTCase1()
-
+    def nTCase(self,t):
+        
+        if t < (1 / self.lambdda) and self.m == 0:
+            return 0
+        elif self.lambdda > self.meu:
+            if t < self.ti:
+                x1, x2 = symbols('x1, x2')
+                x1 = solve(Eq(self.lambdda * t, x1))[0]
+                x2 = solve(Eq((self.meu * t) - (self.meu / self.lambdda), x2))[0]
+                return math.floor(x1) - math.floor(x2)
+                # return self.M + math.floor(self.λ * t) - math.floor((self.μ * t) - (self.μ / self.λ))
+            else:
+                if self.is_departure(t):
+                    if not self.is_arrival(t):
+                        # one left and no one came
+                        return self.k - 2
+                    else:
+                        # one left and one came at the same second
+                        return self.k-1
+                elif self.is_arrival(t):
+                    # no one left but one arrived
+                    return self.k - 1
+                else:
+                    # no one left or arrived n(t) stays the same
+                    return self.nTCase(t-1)
         else:
-            return self.nTCase2()
+            if self.lambdda == self.meu:
+                if self.m == 0:
+                    return 1
+                else:
+                    return self.m
+            else:
+                if t < self.ti:
+                    return self.m + math.floor(self.lambdda * t) - math.floor(self.meu * t)
+                else:
+                    if (t % (1/self.lambdda)) < (1/self.meu):
+                        return 1
+                    else:
+                        return 0
 
-    def nTCase1(self):
-        nt = []
-        i = 0
-        l = int(1/self.lambdda)
-        for t in range(l, self.ti+(l*5), l):
-            nt.append(int(t*self.lambdda)-(int((t*self.meu)-(self.meu/self.lambdda))))
-            if(nt[i] >= self.k):
-                nt[i] = self.k-1
-            i += 1
-        return nt
+    def is_departure(self, t):
+        return (t % (1/self.meu)) == (1/self.lambdda)
 
-    def nTCase2(self):
-        nt = []
-        i = 0
-        l = int(1/self.lambdda)
-        rang = int(self.ti+(l*5))
-        for t in range(0, rang, l):
-            nt.append(int(self.m+(self.lambdda*t)-(self.meu*t)))
-            if(t > 10):
-                nt[i] = 1
-            i += 1
-        return nt
+    def is_arrival(self, t):
+        return t % (1/self.lambdda) == 0
+
 
     #calculate wq(n)
     def wqN(self, n):
@@ -119,44 +137,213 @@ class Deter:
 class sochasitc:
     lambdaa=0
     meu=0
+    c=0
     k=0
     def L(self):
-        if(k==0):
+        if(self.k==0 and self.c==1):
             return int((self.lambdaa)/(self.meu-self.lambdaa))
-        else:
+        elif(self.k!=0 and self.c==1):
             if(self.lambdaa/self.meu!=1):
-                numerator= 1-((k+1)*self.raw(k))+(k*self.raw(k+1))
-                denominator=(1-self.raw(1))*(1-self.raw(k+1))
+                numerator= 1-((self.k+1)*self.raw(self.k))+(self.k*self.raw(self.k+1))
+                denominator=(1-self.raw(1))*(1-self.raw(self.k+1))
                 return self.raw(1)*(numerator/denominator)
+        elif(self.k==0 and self.c>1):
+            return self.Lq()+self.R()
+        elif(self.k!=0 and self.c>1):
+            total=0
+            for n in range(0,self.c):
+                total+= (self.c-n)*((self.R()**n)/math.factorial(n))
+            return self.Lq()+self.c-(self.Pk0MMC()*total)
+
     def Lq(self):
-        if(k==0):
+        if(self.k==0 and self.c==1):
             return (self.lambdaa*self.lambdaa)/(self.meu*(self.meu-self.lambdaa))
-        else:
+        elif(self.k!=0 and self.c==1):
             return self.lambdaa*(1-self.Pk())*self.Wq()
+        elif(self.k==0 and self.c>1):
+            
+            return (((self.R()**(self.c+1))/self.c)/(math.factorial(self.c)*(1-self.R()/self.c)**2))*self.P0MMC()
+        elif(self.k!=0 and self.c>1):
+            total=0
+            for n in range(self.c+1,self.k+1):
+                total+=(n-self.c)*self.PkMMC(n)
+            return total
 
     def Wq(self):
-        if(k==0):
+        if(self.k==0 and self.c==1):
             return (self.lambdaa/(self.meu*(self.meu-self.lambdaa)))
-        else:
+        elif(self.k!=0 and self.c==1):
             return self.W()-(1/self.meu)
+        elif(self.k==0 and self.c>1):
+            return self.Lq()/self.lambdaa
+        elif(self.k!=0 and self.c>1):
+            return self.Lq()/(self.lambdaa*(1-self.PkMMC(self.k)))
 
     def W(self):
-        if(k==0):
+        if(self.k==0 and self.c==1):
             return int(1/(self.meu-self.lambdaa))
-        else:
+        elif(self.k!=0 and self.c==1):
             return self.L()/(self.lambdaa*(1-self.Pk()))
+        elif(self.k==0 and self.c>1):
+            return self.Wq()+(1/self.meu)
+        elif(self.k!=0 and self.c>1):
+            return self.L()/(self.lambdaa*(1-self.PkMMC(self.k)))
 
     def raw(self,num):
         return (self.lambdaa/self.meu)**num
+        
     
     def Pk(self):
         if(self.raw(1)!=1):
-            return self.raw(k)*((1-self.raw(1))/(1-self.raw(k+1)))
+            return self.raw(self.k)*((1-self.raw(1))/(1-self.raw(self.k+1)))
         else:
-            return 1/(k+1)
-soch=sochasitc()
-soch.lambdaa=2
-soch.meu=2.4
-k=5
+            return 1/(self.k+1)
+    #M/M/C
+    def PMMC(self,n):
+        if(n<self.c):
+            return ((self.lambdaa**n)/(math.factorial(n)*(self.meu**n)))*self.P0MMC()
+        else:
+            return ((self.lambdaa**n)/((self.c**(n-self.c))*math.factorial(self.c)*self.meu**n))*self.P0MMC()
+    #M/M/C
+    def P0MMC(self):
 
-print(soch.Lq())
+        total=0
+        if(self.R()/self.c<1):
+            for n in range(0,self.c):
+               total+=(self.R()**n)/math.factorial(n)
+
+            eqf=total+((self.c*self.R()**self.c)/(math.factorial(self.c)*(self.c-self.R())))
+
+            return 1/eqf
+        else:
+            for n in range(0,self.c):
+               total+=(self.R()**n)/math.factorial(n)
+            eqf=total+((1/math.factorial(self.c))*(self.R()**self.c)*((self.c*self.meu)/((self.c*self.meu)-self.lambdaa)))
+            return 1/eqf
+    #M/M/C/K
+    def PkMMC(self,n):
+        if(n<self.c and n>=0):
+            return ((self.R()**n)/math.factorial(n))*self.Pk0MMC()
+        elif(n>= self.c):
+            return ((self.R()**n)/((self.c**(n-self.c))*math.factorial(self.c)))*self.Pk0MMC()
+
+    #M/M/C/K
+    def Pk0MMC(self):
+        total=0
+        for n in range(0,self.c):
+            total+=(self.R()**n)/math.factorial(n)
+        if(self.raw2(1)!=1):
+            return 1/(total+(((self.R()**self.c)/math.factorial(self.c))*((1-self.raw2(self.k-self.c+1))/(1-self.raw2(1)))))
+        else:
+            return 1/(total+(((self.R()**self.c)/math.factorial(self.c))*(self.k-self.c+1)))
+   
+    def R(self):
+        return self.lambdaa/self.meu
+
+    def Ci(self):
+        return self.c-self.R()
+    def raw2(self,num):
+        return (self.R()/self.c)**num
+
+
+
+
+
+
+deter=Deter()
+deter.k=5
+deter.lambdda=1/4
+deter.meu=1/6
+deter.tI()
+print(deter.ti)
+print(deter.nTCase(44))
+
+
+
+
+class DQ:
+    def _init_(self, λ, μ, k, M):
+        self.λ = λ
+        self.μ = μ
+        self.k = k
+        self.M = M
+        self.ti = int(self.calculate_ti())
+
+    def calculate_ti(self):
+        if self.λ > self.μ:
+            Tik = symbols('Tik')
+            eqn = Eq(((self.λ * Tik) - ((self.μ * Tik) - (self.μ / self.λ))), self.k)
+            Ti = solve(eqn)[0]
+            Tik = Ti
+            kTi = self.k
+            while kTi == self.k:
+                Ti = Tik
+                Tik = Ti - (1 / self.λ)
+                x1, x2 = symbols('x1, x2')
+                x1 = solve(Eq(self.λ * Tik, x1))[0]
+                x2 = solve(Eq((self.μ * Tik) - (self.μ / self.λ), x2))[0]
+                kTi = math.floor(x1)-math.floor(x2)
+
+            return Ti
+        else:
+            if self.λ == self.μ:
+                return 0
+            else:
+                Tik = symbols('Tik')
+                eqn = Eq((self.μ * Tik) - (self.λ * Tik), self.M)
+                Ti = solve(eqn)[0]
+                Tik = Ti
+                MTi = self.M
+                while MTi == self.M:
+                    Ti = Tik
+                    Tik = Ti - (1 / self.λ)
+                    MTi = math.floor(self.μ * Tik) - math.floor(self.λ * Tik)
+                return Ti
+
+    def number_Of_customers(self, t):
+        if t < (1 / self.λ) and self.M == 0:
+            return 0
+        elif self.λ > self.μ:
+            if t < self.ti:
+                x1, x2 = symbols('x1, x2')
+                x1 = solve(Eq(self.λ * t, x1))[0]
+                x2 = solve(Eq((self.μ * t) - (self.μ / self.λ), x2))[0]
+                return math.floor(x1) - math.floor(x2)
+                # return self.M + math.floor(self.λ * t) - math.floor((self.μ * t) - (self.μ / self.λ))
+            else:
+                if self.is_departure(t):
+                    if not self.is_arrival(t):
+                        # one left and no one came
+                        return self.k - 2
+                    else:
+                        # one left and one came at the same second
+                        return self.k-1
+                elif self.is_arrival(t):
+                    # no one left but one arrived
+                    return self.k - 1
+                else:
+                    # no one left or arrived n(t) stays the same
+                    return self.number_Of_customers(t-1)
+        else:
+            if self.λ == self.μ:
+                if self.M == 0:
+                    return 1
+                else:
+                    return self.M
+            else:
+                if t < self.ti:
+                    return self.M + math.floor(self.λ * t) - math.floor(self.μ * t)
+                else:
+                    if (t % (1/self.λ)) < (1/self.μ):
+                        return 1
+                    else:
+                        return 0
+
+    def is_departure(self, t):
+        return (t % (1/self.μ)) == (1/self.λ)
+
+    def is_arrival(self, t):
+        return t % (1/self.λ) == 0
+
+    def waiting_time(self, t):
+        pass
